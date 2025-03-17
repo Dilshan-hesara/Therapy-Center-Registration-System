c2c3f3acbffa4a44efd1eea4810307412705d04f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-17 17:48:48 +0530

    config

---
+++ src/main/java/lk/cw/config/FactoryConfiguration.java
@@ -0,0 +1,30 @@
+package lk.cw.config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class FactoryConfiguration {
+    private static FactoryConfiguration factoryConfiguration;
+    private SessionFactory sessionFactory;
+
+    private FactoryConfiguration() {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+//        configuration.addAnnotatedClass(User.class);
+//                .addAnnotatedClass(Item.class)
+//                .addAnnotatedClass(Order.class)
+//                .addAnnotatedClass(OrderDetails.class);
+        sessionFactory = configuration.buildSessionFactory();
+    }
+
+    public static FactoryConfiguration getInstance() {
+        return (factoryConfiguration == null) ?
+                factoryConfiguration = new FactoryConfiguration() :
+                factoryConfiguration;
+    }
+
+    public Session getSession() {
+        return sessionFactory.openSession();
+    }
+}
