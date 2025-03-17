fb889bfb230a5b44ae4ca0c9623cbaedc2b042d2
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-17 17:47:09 +0530

    Reg From

---
+++ src/main/java/lk/cw/controller/RegController.java
@@ -0,0 +1,38 @@
+package lk.cw.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class RegController {
+
+    @FXML
+    private AnchorPane ancer;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private PasswordField txtRePassword;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void BackOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void regisOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void roleComb(ActionEvent event) {
+
+    }
+
+}
