b2e82f1532f7ea5a368f52bf9153641ce414e04c
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-21 09:17:30 +0530

    FactoryConfiguration add hibernate prproties

--- src/main/java/lk/cw/config/FactoryConfiguration.java
+++ src/main/java/lk/cw/config/FactoryConfiguration.java
@@ -1,6 +1,6 @@
 package lk.cw.config;
 
-import lk.cw.entity.User;
+import lk.cw.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -19,19 +19,37 @@ public class FactoryConfiguration {
 //
 //        configuration.configure();
 
-        Configuration configuration = new Configuration();
-        Properties properties = new Properties();
 
-        // Load properties
-        properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
-        configuration.setProperties(properties);
-        configuration.addAnnotatedClass(User.class);
+  try {
+      Configuration configuration = new Configuration();
+      Properties properties = new Properties();
+
+      // Load properties
+      properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("hibernate.properties"));
+      configuration.setProperties(properties);
+
+      configuration.addAnnotatedClass(User.class);
+
+        configuration.addAnnotatedClass(Patient.class);
+       configuration.addAnnotatedClass(Therapist.class);
+       configuration.addAnnotatedClass(TherapyProgram.class);
+       configuration.addAnnotatedClass(TherapySession.class);
+       configuration.addAnnotatedClass(Payment.class);
+
 
 //        configuration.addAnnotatedClass(User.class);
 //                .addAnnotatedClass(Item.class)
 //                .addAnnotatedClass(Order.class)
 //                .addAnnotatedClass(OrderDetails.class);
-        sessionFactory = configuration.buildSessionFactory();
+      sessionFactory = configuration.buildSessionFactory();
+  }
+
+
+  catch (IOException e) {
+        throw new IOException("Failed to load hibernate.properties", e);
+    } catch (Exception e) {
+        throw new RuntimeException("Failed to create SessionFactory", e);
+    }
     }
 
     public static FactoryConfiguration getInstance() throws IOException {
