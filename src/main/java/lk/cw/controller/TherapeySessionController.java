3cc1cc1e7899be4004ab1aeb05c5bb8af2f8df4f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-21 09:15:44 +0530

    therapiy session Manage view

---
+++ src/main/java/lk/cw/controller/TherapeySessionController.java
@@ -0,0 +1,116 @@
+package lk.cw.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class TherapeySessionController {
+
+    @FXML
+    private TableView<?> SessionTable;
+
+    @FXML
+    private Button btndelete;
+
+    @FXML
+    private Button btnsave;
+
+    @FXML
+    private Button btnupdate;
+
+    @FXML
+    private TableColumn<?, ?> coldate;
+
+    @FXML
+    private TableColumn<?, ?> colid;
+
+    @FXML
+    private TableColumn<?, ?> colstatus;
+
+    @FXML
+    private TableColumn<?, ?> coltherapist;
+
+    @FXML
+    private TableColumn<?, ?> coltherapist1;
+
+    @FXML
+    private TableColumn<?, ?> coltime;
+
+    @FXML
+    private ComboBox<?> combostatus;
+
+    @FXML
+    private ComboBox<?> combotherapistId;
+
+    @FXML
+    private DatePicker datepicker;
+
+    @FXML
+    private Label lbldate;
+
+    @FXML
+    private Label lblid;
+
+    @FXML
+    private Label lblregister;
+
+    @FXML
+    private Label lblstatus;
+
+    @FXML
+    private Label lbltherapistid;
+
+    @FXML
+    private Label lbltherapistname;
+
+    @FXML
+    private TextField txttime;
+
+    @FXML
+    void ComboStatusOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ComboTherapistIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatePickerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TableOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void UpdateOnAction(ActionEvent event) {
+
+    }
+
+}
