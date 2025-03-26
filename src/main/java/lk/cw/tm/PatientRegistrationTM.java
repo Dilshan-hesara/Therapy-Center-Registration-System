2b09230fea040068bb46a328d71ae7d4c519d206
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 07:08:47 +0530

    createTM

---
+++ src/main/java/lk/cw/tm/PatientRegistrationTM.java
@@ -0,0 +1,4 @@
+package lk.cw.tm;
+
+public class PatientRegistrationTM {
+}
---
+++ src/main/java/lk/cw/tm/TherapySessionTM.java
@@ -0,0 +1,19 @@
+package lk.cw.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TherapySessionTM {
+    private String sessionId;
+    private Date sessionDate;
+    private String sessionTime;
+    private String status;
+    private String therapistId;
+    private String patientId;
+}
