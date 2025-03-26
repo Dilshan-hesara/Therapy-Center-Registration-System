72114108ded9a807872aeede0be74db7b4aec4d7
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 20:14:54 +0530

    Therapist TM up

--- src/main/java/lk/cw/tm/TherapistTM.java
+++ src/main/java/lk/cw/tm/TherapistTM.java
@@ -1,12 +1,12 @@
 package lk.cw.tm;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Getter
+@Setter
 public class TherapistTM {
     private String therapistId;
     private String therapistName;
