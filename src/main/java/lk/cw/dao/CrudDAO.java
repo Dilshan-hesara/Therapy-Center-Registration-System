019e9724c98fed6bfa283471aeba86098a20f87b
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 09:20:18 +0530

    ADD Crud Davo

--- src/main/java/lk/cw/dao/CrudDAO.java
+++ src/main/java/lk/cw/dao/CrudDAO.java
@@ -1,4 +1,13 @@
 package lk.cw.dao;
 
-public interface CrudDAO {
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CrudDAO<T> extends SuperDAO {
+    String getNextId() throws SQLException, IOException;
+    List<T> getAll() throws SQLException, IOException;
+    boolean save(T entity) throws SQLException, IOException;
+    boolean update(T entity) throws SQLException, IOException;
+    boolean delete(String ID) throws SQLException, IOException;
 }
