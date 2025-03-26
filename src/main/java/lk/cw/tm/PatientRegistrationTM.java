6b9a1afaf51156c1db4900eb03e28bae8f442090
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-27 05:29:55 +0530

    pation TM UP

--- src/main/java/lk/cw/tm/PatientRegistrationTM.java
+++ src/main/java/lk/cw/tm/PatientRegistrationTM.java
@@ -1,4 +1,20 @@
 package lk.cw.tm;
 
+import lombok.*;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
 public class PatientRegistrationTM {
+    private String registrationId;
+    private String patientId;
+    private String programId;
+    private Date registrationDate;
+    private int sessionCount;
+
+
 }
