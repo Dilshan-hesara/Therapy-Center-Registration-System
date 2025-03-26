cb506d768ee548a266a9857249d0a4363dd10da3
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 07:08:08 +0530

    user UI UP

---
+++ src/main/java/lk/cw/tm/UserTM.java
@@ -0,0 +1,16 @@
+package lk.cw.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Data
+    public class UserTM {
+        private String UserID;
+        private String UserName;
+        private String Password;
+        private String Role;
+    }
+
