dd6361a6756a4cbf9a0a5e411f09397944362738
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 07:09:40 +0530

    payment up

--- src/main/java/lk/cw/controller/PaymentController.java
+++ src/main/java/lk/cw/controller/PaymentController.java
@@ -1,4 +1,99 @@
 package lk.cw.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class PaymentController {
+
+    @FXML
+    private TableView<?> PaymentTable;
+
+    @FXML
+    private Button btndelete;
+
+    @FXML
+    private Button btnsave;
+
+    @FXML
+    private Button btnupdate;
+
+    @FXML
+    private TableColumn<?, ?> colamount;
+
+    @FXML
+    private TableColumn<?, ?> coldate;
+
+    @FXML
+    private TableColumn<?, ?> colpatid;
+
+    @FXML
+    private TableColumn<?, ?> colpaymentid;
+
+    @FXML
+    private TableColumn<?, ?> colstatus;
+
+    @FXML
+    private ComboBox<?> combopatientid;
+
+    @FXML
+    private DatePicker datepicker;
+
+    @FXML
+    private Label lblPatientid;
+
+    @FXML
+    private Label lbldate;
+
+    @FXML
+    private Label lblid;
+
+    @FXML
+    private Label lblstatus;
+
+    @FXML
+    private TextField txtamount;
+
+    @FXML
+    void ComboPatientIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatePickerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TableOnClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void UpdateOnAction(ActionEvent event) {
+
+    }
+
 }
