0958c3871d6ad780f9f99be18c0e21bfe15e15f0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 09:29:48 +0530

    Therapist add BOFactory

--- src/main/java/lk/cw/bo/BOFactory.java
+++ src/main/java/lk/cw/bo/BOFactory.java
@@ -1,6 +1,7 @@
 package lk.cw.bo;
 
 import lk.cw.bo.custom.impl.PatientBOImpl;
+import lk.cw.bo.custom.impl.TherapistBOImpl;
 import lk.cw.bo.custom.impl.UserBOImpl;
 
 public class BOFactory {
@@ -14,17 +15,19 @@ public class BOFactory {
     public enum BOTypes{
         PATIENT, PAYMENT,THERAPIST,THERAPYOROGRAM,THERAPYSESSION,USER
     }
-    //Object creation logic for BO objects
+
     public SuperBO getBO(BOTypes types){
         switch (types){
 
             case USER:
-                return (SuperBO) new UserBOImpl();
+                return  new UserBOImpl();
 
             case PATIENT:
-                return (SuperBO) new PatientBOImpl();
+                return  new PatientBOImpl();
 
 
+            case THERAPIST :
+                return  new TherapistBOImpl();
 
 
             default:
