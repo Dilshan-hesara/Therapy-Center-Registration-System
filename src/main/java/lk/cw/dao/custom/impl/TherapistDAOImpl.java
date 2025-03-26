2e6208f46d3a27be2721747aae66483cc78f6942
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 20:15:30 +0530

    Therapist DAOIMPL up

--- src/main/java/lk/cw/dao/custom/impl/TherapistDAOImpl.java
+++ src/main/java/lk/cw/dao/custom/impl/TherapistDAOImpl.java
@@ -1,35 +1,121 @@
 package lk.cw.dao.custom.impl;
 
+import lk.cw.config.FactoryConfiguration;
 import lk.cw.dao.custom.TherapistDAO;
 import lk.cw.entity.Therapist;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class TherapistDAOImpl implements TherapistDAO {
+
+
     @Override
     public String getNextId() throws SQLException, IOException {
-        return "";
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = "SELECT l.therapistId FROM Therapist l ORDER BY l.therapistId DESC";
+        Query<String> query = session.createQuery(hql);
+        query.setMaxResults(1);
+        String lastId = query.uniqueResult();
+
+        transaction.commit();
+        session.close();
+
+        if (lastId != null) {
+            String substring = lastId.substring(1);
+            int i = Integer.parseInt(substring);
+            int newIdIndex = i + 1;
+            return String.format("T%03d", newIdIndex);
+        }
+
+        return "T001";
     }
 
     @Override
     public List<Therapist> getAll() throws SQLException, IOException {
-        return List.of();
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Query<Therapist> query = session.createQuery("SELECT p FROM Therapist p", Therapist.class);
+        List<Therapist> therapists = query.list();
+
+        transaction.commit();
+        session.close();
+        return therapists;
     }
 
     @Override
-    public boolean save(Therapist entity) throws SQLException, IOException {
-        return false;
+    public boolean save(Therapist therapist) throws SQLException, IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(therapist);
+        transaction.commit();
+        session.close();
+        return true;
     }
 
     @Override
-    public boolean update(Therapist entity) throws SQLException, IOException {
-        return false;
+    public boolean update(Therapist therapist) throws SQLException, IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        session.update(therapist);
+
+        transaction.commit();
+        session.close();
+
+        return true;
     }
 
     @Override
     public boolean delete(String ID) throws SQLException, IOException {
-        return false;
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+
+        Therapist therapist = new Therapist();
+        therapist.setTherapistId(ID);
+        session.remove(therapist);
+
+
+        transaction.commit();
+        session.close();
+
+        return true;
+    }
+
+    @Override
+    public Therapist findById(String therapistId) throws SQLException, ClassNotFoundException {
+        Therapist therapist = null;
+
+        try (Session session = FactoryConfiguration.getInstance().getSession()) {
+            // Fetch the Student entity using the primary key
+            therapist = session.get(Therapist.class, therapistId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to fetch the student by ID: " + therapistId);
+        }
+
+        return therapist;
+    }
+
+
+    @Override
+    public ArrayList<String> getAllTherapistIDs() throws SQLException, ClassNotFoundException, IOException {
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        String hql = ("SELECT therapistId FROM Therapist");
+
+        Query query = session.createQuery(hql);
+        ArrayList<String> list = (ArrayList<String>) query.list();
+
+        transaction.commit();
+        session.close();
+        return list;
     }
 }
