cb6508970bd76e724aff136bbc980106e3e99c60
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-27 05:29:36 +0530

    Patitent reg DAO ,IMPL up

--- src/main/java/lk/cw/dao/custom/PatientRegDAO.java
+++ src/main/java/lk/cw/dao/custom/PatientRegDAO.java
@@ -3,5 +3,10 @@ package lk.cw.dao.custom;
 import lk.cw.dao.CrudDAO;
 import lk.cw.entity.Patient_Registration;
 
+import java.sql.SQLException;
+
 public interface PatientRegDAO extends CrudDAO<Patient_Registration> {
+
+    public Patient_Registration findById(String patientId) throws SQLException, ClassNotFoundException;
+
 }
