f87e959d2bb681fb1a95d91e5425fcc6f1384083
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-03-26 20:15:40 +0530

    Therapist BO up

--- src/main/java/lk/cw/bo/custom/TherapistBO.java
+++ src/main/java/lk/cw/bo/custom/TherapistBO.java
@@ -1,6 +1,21 @@
 package lk.cw.bo.custom;
 
 import lk.cw.bo.SuperBO;
+import lk.cw.dto.TherapistDTO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface TherapistBO extends SuperBO {
+    public boolean save(TherapistDTO therapistDTO) throws IOException, SQLException;
+    public String getNextId() throws SQLException, IOException;
+    public TherapistDTO findById(String therapistId) throws SQLException, ClassNotFoundException;
+    public ArrayList<String> getAllTherapistIDs() throws SQLException, ClassNotFoundException, IOException;
+    public List<TherapistDTO> getAll() throws SQLException, IOException;
+    public boolean update(TherapistDTO therapistDTO) throws IOException, SQLException;
+    public boolean delete(String ID) throws SQLException, IOException;
+
+
 }
