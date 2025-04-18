7c1b9468b28126758a38e096428b382483b988d0
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-19 01:43:13 +0530

    payment dto up

--- src/main/java/lk/cw/dto/PaymentDTO.java
+++ src/main/java/lk/cw/dto/PaymentDTO.java
@@ -9,18 +9,22 @@ import java.sql.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class PaymentDTO {
-    private String paymentId;
-    private String patientId;
-    private double amount;
-    private Date paymentDate;
-    private String Status;
 
-    public PaymentDTO(String paymentId, String patientId, double amount, String paymentDate, String status) {
-        this.paymentId = paymentId;
-        this.patientId = patientId;
-        this.amount = amount;
-        this.paymentDate = Date.valueOf(paymentDate);
-        this.Status = status;
+
+    public class PaymentDTO {
+        private String paymentId;
+        private String patientId;
+        private double amount;
+        private Date paymentDate;
+        private String Status;
+
+        public PaymentDTO(String paymentId, String patientId, double amount, String paymentDate, String status) {
+            this.paymentId = paymentId;
+            this.patientId = patientId;
+            this.amount = amount;
+            this.paymentDate = Date.valueOf(paymentDate);
+            this.Status = status;
+        }
     }
-}
+
+
