300b1544f2e0b9c2c1ce7c651c26cb0990a83199
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-19 01:43:05 +0530

    payment tm

---
+++ src/main/java/lk/cw/tm/PaymentTM.java
@@ -0,0 +1,22 @@
+package lk.cw.tm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PaymentTM {
+    private String paymentId;
+    private String patientId;
+    private double amount;
+    private Date paymentDate;
+    private String Status;
+
+
+}
+
+
