411bbdc5c43c93a7b5c881d78da43671ee04776d
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-21 13:08:03 +0530

    therapy session up

---
+++ src/main/java/lk/cw/bo/custom/TherapySessionBO.java
@@ -0,0 +1,17 @@
+package lk.cw.bo.custom;
+
+import lk.cw.bo.SuperBO;
+import lk.cw.dto.TherapySessionDTO;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface TherapySessionBO extends SuperBO {
+
+    public boolean save(TherapySessionDTO therapySessionDTO) throws IOException, SQLException;
+    public String getNextId() throws SQLException, IOException;
+    public List<TherapySessionDTO> getAll() throws SQLException, IOException;
+    public boolean update(TherapySessionDTO therapySessionDTO) throws IOException, SQLException;
+    public boolean delete(String ID) throws SQLException, IOException;
+}
