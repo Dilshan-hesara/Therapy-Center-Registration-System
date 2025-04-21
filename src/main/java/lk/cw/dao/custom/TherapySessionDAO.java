2e521c394fbd7c1df13f9cd7975600cc07fd8fe2
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-21 13:08:08 +0530

    therapy session up

---
+++ src/main/java/lk/cw/dao/custom/TherapySessionDAO.java
@@ -0,0 +1,8 @@
+package lk.cw.dao.custom;
+
+import lk.cw.dao.CrudDAO;
+import lk.cw.entity.Therapy_Session;
+import lk.cw.tm.TherapySessionTM;
+
+public interface TherapySessionDAO extends CrudDAO <Therapy_Session>{
+}
