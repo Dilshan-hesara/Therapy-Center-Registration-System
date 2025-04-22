3ecb5400ae0732441e627dacd262a571f8107c0a
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-22 09:36:04 +0530

    create add payment

--- src/main/java/lk/cw/controller/AddPaymentConteoller.java
+++ src/main/java/lk/cw/controller/AddPaymentConteoller.java
@@ -1,4 +1,79 @@
 package lk.cw.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class AddPaymentConteoller {
+
+    @FXML
+    private Button btndelete;
+
+    @FXML
+    private Button btnsave;
+
+    @FXML
+    private ComboBox<?> comboStatus;
+
+    @FXML
+    private ComboBox<?> combopatientid;
+
+    @FXML
+    private DatePicker datepicker;
+
+    @FXML
+    private Label lblPatientid;
+
+    @FXML
+    private Label lblPatientid1;
+
+    @FXML
+    private Label lblbalance;
+
+    @FXML
+    private Label lbldate;
+
+    @FXML
+    private Label lblid;
+
+    @FXML
+    private Label lblstatus;
+
+    @FXML
+    private TextField txtamount;
+
+    @FXML
+    void ComboPatientIdOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DatePickerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void StatusComboOnAction(ActionEvent event) {
+
+    }
+
 }
