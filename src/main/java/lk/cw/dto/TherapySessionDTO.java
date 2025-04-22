398e15145b6ed8d568a78b5c9a0ed018e301c48f
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-22 21:25:24 +0530

    sesson dto up

--- src/main/java/lk/cw/dto/TherapySessionDTO.java
+++ src/main/java/lk/cw/dto/TherapySessionDTO.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
+import java.util.ArrayList;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -17,13 +18,27 @@ public class TherapySessionDTO {
     private String therapistId;
     private String patientId;
 
-    public TherapySessionDTO(String sessionId, String sessionDate, String sessionTime, String status, String therapistId, String patientId) {
-        this.sessionId = sessionId;
+    private ArrayList<PaymentDTO>paymentDTOS;
+
+    public TherapySessionDTO(String sessionId, String sessionDate, String sessionTime, String status, String therapistId, String patientId, ArrayList<PaymentDTO> paymentDTOS) {
+
+                this.sessionId = sessionId;
         this.sessionDate = Date.valueOf(sessionDate);
         this.sessionTime = sessionTime;
         this.status = status;
         this.therapistId = therapistId;
         this.patientId = patientId;
+        this.paymentDTOS = paymentDTOS;
 
     }
+
+//    public TherapySessionDTO(String sessionId, String sessionDate, String sessionTime, String status, String therapistId, String patientId) {
+//        this.sessionId = sessionId;
+//        this.sessionDate = Date.valueOf(sessionDate);
+//        this.sessionTime = sessionTime;
+//        this.status = status;
+//        this.therapistId = therapistId;
+//        this.patientId = patientId;
+//
+//    }
 }
