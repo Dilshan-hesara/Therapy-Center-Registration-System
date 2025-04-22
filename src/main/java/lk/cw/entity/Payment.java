cf8266dabef54d25fc13bf28e9e51eb12a0220fe
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-22 21:25:49 +0530

    payment entity up

--- src/main/java/lk/cw/entity/Payment.java
+++ src/main/java/lk/cw/entity/Payment.java
@@ -17,7 +17,7 @@ public class Payment implements SuperEntity{
     @Id
     private String paymentId;
     private double amount;
-    private Date paymentDate;
+    private String paymentDate;
     private String Status;
 
     @ManyToOne
@@ -25,12 +25,39 @@ public class Payment implements SuperEntity{
     private Patient patient;
 
 
-    public Payment(String paymentId, Patient patient, double amount, Date paymentDate, String status) {
+    public Payment(String paymentId, Patient patient, double amount, String paymentDate, String status) {
         this.paymentId = paymentId;
         this.patient = patient;
         this.amount = amount;
         this.paymentDate = paymentDate;
         Status = status;
+
+    }
+
+
+    public Payment(String paymentId, String patientId, double amount, String paymentDate, String status) {
+        this.paymentId = paymentId;
+        this.patient = patient;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        Status = status;
+
+    }
+
+    public Payment(String paymentId, String status, double amount, Date paymentDate, Patient patient) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.paymentDate = paymentDate.toString();
+        this.patient = patient;
+        this.Status = status;
+    }
+
+    public Payment(String paymentId, String status, double amount, String paymentDate, Patient patient) {
+        this.paymentId = paymentId;
+        this.amount = amount;
+        this.paymentDate = paymentDate;
+        this.patient = patient;
+        this.Status = status;
 
     }
 }
