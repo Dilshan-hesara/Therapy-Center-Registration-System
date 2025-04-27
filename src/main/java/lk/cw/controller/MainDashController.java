b13fd56f3fdbd2ec8a69d5f3ca850dea48e93809
Dilshan-hesara <hesarsdilshan@gmail.com>
2025-04-27 06:23:58 +0530

    dash board up

--- src/main/java/lk/cw/controller/MainDashController.java
+++ src/main/java/lk/cw/controller/MainDashController.java
@@ -1,4 +1,82 @@
 package lk.cw.controller;
 
-public class MainDashController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import lk.cw.bo.BOFactory;
+import lk.cw.bo.custom.PatientBO;
+import lk.cw.bo.custom.TherapistBO;
+import lk.cw.bo.custom.TherapyProgramBO;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class MainDashController implements Initializable {
+
+
+
+    @FXML
+    private Label lbltotalpatient;
+
+    @FXML
+    private Label lblTherpist;
+
+
+    @FXML
+    private Label lblpro;
+
+    PatientBO patientBO = (PatientBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.PATIENT);
+    TherapyProgramBO therapyProgramBO = (TherapyProgramBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.THERAPYOROGRAM);
+    TherapistBO therapistBO = (TherapistBO) BOFactory.getBoFactory().getBO(BOFactory.BOTypes.THERAPIST);
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        loadPatientCount();
+        loadTherapistCount();
+        loadProgramCount();
+    }
+
+
+    private void loadPatientCount() {
+        try {
+
+            int totalLabors = patientBO.getTotalPatients();
+            lbltotalpatient.setText(String.valueOf(totalLabors));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    private void loadTherapistCount() {
+        try {
+
+            int totalTherapists = therapistBO.getTotalTherapists();
+            lblTherpist.setText(String.valueOf(totalTherapists));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void loadProgramCount() {
+        try {
+
+            int totalPrograms = therapyProgramBO.getTotalPrograms();
+            lblpro.setText(String.valueOf(totalPrograms));
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
